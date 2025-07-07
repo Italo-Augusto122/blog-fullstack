340d62eaf53c33b5b54d870485dd214117bfd5e0
Anarquia122 <italogusto823@gmail.com>
2025-07-07 01:20:19 -0300

    character page, builds section

--- front-end/src/js/character.js
+++ front-end/src/js/character.js
@@ -16,7 +16,7 @@ fetch("../json/characters.json")
 
             document.getElementById("element").innerHTML = `<img class="char_banner" src="../images/elements/${char.element}.png" alt="${char.element}" style="max-width: 25px;" >`;
 
-            document.getElementById("char_banner").innerHTML = `<img class="char_banner" src="../images/characters/${char.name}/${char.name}-uncolored-banner.png" alt="${char.name}" >`;
+            document.getElementById("char_banner").innerHTML = `<img class="char_banner" src="../images/characters/${char.name}/${char.name}-uncolored-banner.webp" alt="${char.name}" >`;
 
             document.getElementById("char_description").textContent = char.description;
 
@@ -24,12 +24,54 @@ fetch("../json/characters.json")
 
             document.getElementById("char_stats").textContent = `${char.fullName} Base Stats`;
 
-            document.getElementById("char_hp").textContent = char.baseStats[0];
-            document.getElementById("char_atk").textContent = char.baseStats[1];
-            document.getElementById("char_def").textContent = char.baseStats[2];
-            document.getElementById("char_impact").textContent = char.baseStats[3];
-            document.getElementById("char_maestry").textContent = char.baseStats[4];
-            document.getElementById("char_proficiency").textContent = char.baseStats[5];
+            vect = [
+                "hp",
+                "atk",
+                "def",
+                "impact",
+                "maestry",
+                "proficiency"
+            ]
+            for (i = 0; i < char.baseStats.length; i++) {
+                document.getElementById("char_" + vect[i]).textContent = char.baseStats[i];
+            }
+
+            const pageWEngines = () => {
+                document.getElementById("container_builds").innerHTML = "";
+
+                const box = document.createElement("div");
+                box.className = "w-engine_box";
+
+                const text = document.createElement("p");
+                text.textContent = char.wEngines[0];
+
+                box.appendChild(text);
+                document.getElementById("container_builds").appendChild(box);
+            }
+            const pageTeams = () => {
+                document.getElementById("container_builds").innerHTML = "";
+
+                const box = document.createElement("div");
+                box.className = "teams_box";
+
+                const text = document.createElement("p");
+                text.textContent = char.teams[0];
+
+                box.appendChild(text);
+                document.getElementById("container_builds").appendChild(box);
+            }
+
+            const changePage = (index) => {
+                if (index == 0) {
+                    pageWEngines();
+                } else if (index == 1) {
+                    pageTeams();
+                }
+            }
+
+            pageWEngines()
+            document.getElementById("page_0").addEventListener("click", () => {changePage(0)});
+            document.getElementById("page_1").addEventListener("click", () => {changePage(1)});
         } else {
             document.body.innerHTML = "<h1>Personagem não encontrado!</h1>";
         }
